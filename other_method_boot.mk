d0a392da2de17b9d5a6cdee8917adacad25ee05f
patraanjan23 <patraanjan23@gmail.com>
2014-12-06 14:23:29 +0530

    general update in device tree. clean up

---
+++ other_method_boot.mk
@@ -0,0 +1,13 @@
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
+		$(recovery_ramdisk) \
+		$(recovery_kernel)
+	@echo ----- Making recovery image ------
+	perl device/mtk/repack-MT65xx.pl -recovery $(recovery_kernel) out/target/product/a106/recovery/root $@ $(MKBOOTIMG)
+	@echo ----- Made recovery image -------- $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+
+
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
+	$(call pretty,"Target boot image: $@")
+	perl device/mtk/repack-MT65xx.pl -boot $(recovery_kernel) out/target/product/a106/root $@ $(MKBOOTIMG)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
